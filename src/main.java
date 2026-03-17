b7e6fc1dc7414c51f3bc9407a6b5661c06f931de
lika <likadatvi17@gmail.com>
2026-03-17 05:51:17 -0700

    Conflict B change

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ public class main {
 
 
     public static void main(String[] args) {
-        System.out.println("My name is: Lia");
+        System.out.println("My name is: Barbare");
         System.out.println("your second name is Datunashvili");
 
     }
