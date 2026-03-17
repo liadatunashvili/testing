a4073acede0cfe431c30eaa4a2b885021d2f6d9b
lika <likadatvi17@gmail.com>
2026-03-17 05:59:35 -0700

    Rebase test change
    rebase continue

--- src/main.java
+++ src/main.java
@@ -1,17 +1,9 @@
 public class main {
-
-
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("My name is: Lika");
-=======
         System.out.println("My name is: Barbare");
->>>>>>> conflict-b
         System.out.println("your second name is Datunashvili");
-
         System.out.println("changing main for rebase");
+        System.out.println("rebase testing");
     }
-
-
-
 }
