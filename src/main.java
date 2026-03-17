5caab39b02e1fbd23247ebf1a52ad227b3674b69
lika <likadatvi17@gmail.com>
2026-03-17 05:57:07 -0700

    resolve merge conflict

--- src/main.java
+++ src/main.java
@@ -2,11 +2,9 @@ public class main {
 
 
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("My name is: Lika");
-=======
-        System.out.println("My name is: Barbare");
->>>>>>> conflict-b
+
         System.out.println("your second name is Datunashvili");
 
 
