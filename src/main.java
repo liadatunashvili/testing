cca56127e2f793ac7e2e873ca20173337d8c4984
lika <likadatvi17@gmail.com>
2026-03-17 05:43:01 -0700

    added second name

--- src/main.java
+++ src/main.java
@@ -3,6 +3,7 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("My name is: Lia");
+        System.out.println("your second name is Datunashvili");
 
     }
 
